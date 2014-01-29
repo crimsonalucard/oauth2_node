ff5d7fcdfe2780d7ed27a55ff19fad77e6f1f2e9
byeh <byeh@outboxtechnology.com>
2014-01-28 17:56:47 -0800

    adding commit script...

---
+++ git_push.sh
@@ -0,0 +1,7 @@
+git checkout master_with_security_info
+git pull . master
+git push heroku master_with_security_info:master
+git checkout master
+git push origin master:master
+git checkout master 
+
